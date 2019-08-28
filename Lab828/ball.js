bdef1bc2b42dbb98f8d9e5d59b8f154e7e9d7ac1
Calvin <798119@seq.org>
2019-08-28 08:48:56 -0700

    Vectors

--- Lab828/ball.js
+++ Lab828/ball.js
@@ -2,12 +2,9 @@
 
 class Ball{
   constructor(x,y,dx,dy){
-    this.x=x;
-    this.y=y;
-    this.dx=dx;
-    this.dy = dy;
-    this.clr = color(random(255), random(255),random(255))
-    }
+    this.loc=createVector(x,y);
+    this.vel=createVector(dx, dy);
+  }
 run(){
   this.checkedges();
   this.update();
@@ -15,75 +12,31 @@ run(){
 
 }
 checkedges(){
-  if(this.x<0){
-    this.dx = -this.dx;
+  if(this.loc.x <0){
+    this.vel.x = -this.vel.x;
     }
-    if(this.x > width){
-    this.dx = -this.dx
+    if(this.loc.x > width){
+    this.vel.x = -this.vel.x
     }
-    if(this.y < 0){
-    this.dy = -this.dy
+    if(this.loc.y < 0){
+    this.vel.y = -this.vel.y
     }
-    if(this.y > height){
-    this.dy = -this.dy
-    }
-  }
-
-update(){
-  //this.clr = color(random(255), random(255),random(255))
-this.x = this.x + this.dx;
-this.y = this.y + this.dy;
-}
-render(){
-    //this.clr = color(random(255), random(255),random(255))
-fill(this.clr);
-rect(this.x, this.y, 50,50)
-//ellipse(this.x, this.y, 50,50)
-
-}
-}
-class Triangle{
-  constructor(x,y,z,dx,dy,dz){
-    this.x=x;
-    this.y=y;
-    this.z = z;
-    this.dx=dx;
-    this.dy = dy;
-    this.dz =dz;
-    this.clr = color(random(255), random(255),random(255))
-    }
-run(){
-  this.checkedges();
-  this.update();
-  this.render();
-
-}
-checkedges(){
-  if(this.x<0){
-    this.dx = -this.dx;
-    }
-    if(this.x > width){
-    this.dx = -this.dx
-    }
-    if(this.y < 0){
-    this.dy = -this.dy
-    }
-    if(this.y > height){
-    this.dy = -this.dy
+    if(this.loc.y > height){
+    this.vel.y = -this.vel.y
     }
   }
 
 update(){
-  //this.clr = color(random(255), random(255),random(255))
-this.x = this.x + this.dx;
-this.y = this.y + this.dy;
-this.z =this.z + this.dz;
+  this.clr = color(random(255), random(255),random(255))
+this.loc.add(this.vel)
 }
 render(){
     //this.clr = color(random(255), random(255),random(255))
 fill(this.clr);
-triangle(this.x, this.y, this.dz)
+ellipse(this.loc.x, this.loc.y)
 //ellipse(this.x, this.y, 50,50)
+}
+
 
 }
-}
+//ellipse(this.x, this.y, 50,50)
--- Lab828/sketch.js
+++ Lab828/sketch.js
@@ -3,13 +3,13 @@
 //  This is correct
 
 var balls=[]
-var triangles= []
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
 background(30,30,30);
 loadBalls(150);
-loadtriangles(100);
+
 }
 
 
@@ -29,16 +29,4 @@ function runBalls(){
   for(var i =0;i < balls.length; i++){
     balls[i].run();
   }
-}
-
-function loadtriangles(n){
-  for (var i=0; i < n; i++){
-    triangles[i] = new Triangle(random(width), random(height), random(100), random(-8,8), random(-8,8),random(-8,8))
-  }
-}
-
-function runTriangles(){
-  for(var i =0;i < triangles.length; i++){
-    triangles[i].run();
-}
 }
