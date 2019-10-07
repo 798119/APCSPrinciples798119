f415504241ebe3de0ac23112659bf5b9316b4d7b
Calvin <798119@seq.org>
2019-10-07 08:50:38 -0700

    uyt

---
+++ LAB1007 Checkerboard/Square.js
@@ -0,0 +1,19 @@
+//  Calvin Kapral
+// 	8/15/19
+//  This is correct
+
+
+class Square{
+  constructor(x,y,clr)
+  this.x=x;
+  this.y=y;
+  this.clr=clr;
+}
+run(){
+  this.render();
+}
+
+render()[
+
+
+]
---
+++ LAB1007 Checkerboard/sketch.js
@@ -0,0 +1,25 @@
+//  Calvin Kapral
+// 	8/15/19
+//  This is correct
+var square []
+//variables within code
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+background(5,5,5);
+fill(10,10,10);
+loadsquare();
+
+//Square For Loop
+
+
+for (var i=0; i< squares.length; i++){
+    squares[i].render();
+  }
+
+}
+function loadsquare(){
+ 
+
+
+}
