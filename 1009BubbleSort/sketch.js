a0a6e55dcfb8cab4b2a76da010697ad1a63a4351
Calvin <798119@seq.org>
2019-10-09 08:39:14 -0700

    f

---
+++ 1009BubbleSort/sketch.js
@@ -0,0 +1,17 @@
+//  Calvin Kapral
+// 	8/15/19
+//  This is correct
+//  The setup function function is called once when your program begins
+var list=[1,3,2,9,7];
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+background(5,5,5);
+fill(10,10,10);
+}
+function swap(list,a ,b){
+var temp = list[a];
+list[a]= list[b];
+list[b]=temp;
+console.log(list)
+  }
