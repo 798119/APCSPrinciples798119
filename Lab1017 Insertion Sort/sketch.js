06620c7ad04311f5f7fb9ec515a0772d99af96b0
Calvin <798119@seq.org>
2019-10-21 08:46:32 -0700

    hju

--- Lab1017 Insertion Sort/sketch.js
+++ Lab1017 Insertion Sort/sketch.js
@@ -2,20 +2,25 @@
 // 	8/15/19
 //  This is correct
 //  The setup function function is called once when your program begins
+
 function setup() {
+  var cnv=createCanvas(800,800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(200,200,200);
   var temp;
   var list[3,6,8,4,9]
-        for (var  i = 1; i < arr.length; i++)
- {
-              for(var  j = i ; j > 0 ; j--)
-              {
+}
+  function loadlist(){
+      for (var  i = 1; i < arr.length; i++)
+      for(var  j = i ; j > 0 ; j--)
+}
 //  Swapping Code +++++++++
-                 if(arr[j] < arr[j-1]){
-                    temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+              if(arr[j] < arr[j-1]){
+              temp = arr[j];
+              arr[j] = arr[j-1];
+              arr[j-1] = temp;
                   }
 				//  Swapping Code +++++++++
-              }
-         }
-       }
+
+
+console.log(list)
