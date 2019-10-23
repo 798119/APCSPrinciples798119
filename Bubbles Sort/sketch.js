5b1178f19480372dfbfd6752a9d82b6da8347eb7
798119 <54591234+798119@users.noreply.github.com>
2019-10-23 06:46:54 -0700

    d

--- Bubbles Sort/sketch.js
+++ Bubbles Sort/sketch.js
@@ -5,7 +5,7 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  fill(200, 30, 150);
+  fill(200, 40, 150);
 }
 
 function bubbleSort(array) {
