0e8a9515f8a364808518d085c4011d170ad71be5
Calvin <798119@seq.org>
2019-11-13 08:48:27 -0800

    k

--- Project Snake/sketch.js
+++ Project Snake/sketch.js
@@ -3,24 +3,28 @@
 //  This is correct
 
 //put setup code here
+
+
+
 function setup(){
   var cnv=createCanvas(800,800);
     cnv.position((windowWidth-width)/2,30);
       background(0);
-      frameRate(15);
+      frameRate(10);
 
 snake = new Snake (Math.floor(Math.random()*26)*20,Math.floor(Math.random()*26)*20,20)
 // food= new Food(Math.floor(Math.random((0,40)*20)), Math.floor(Math.random((0,40)*20)));
 food = new Food (Math.floor(Math.random()*26)*20,Math.floor(Math.random()*26)*20)
   }
 
-  function draw(){
-    background (0,0,0);
-    runFood();
-    runSnake();
+
+
+
+
+
+
 
   }
-
   function loadFood(){
 
   }
@@ -36,4 +40,4 @@ food.run();
   function runSnake(){
 snake.run();
   }
-t
+}
